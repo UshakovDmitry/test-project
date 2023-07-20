f74e2d0b3c2e4f501d73526968123da0167c81ac
Dmitry Ushakov <dimau95@yandex.ru>
2023-07-20 20:03:54 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <main class="container">
+   <ComponentStart></ComponentStart>
+ </main>
+</template>
+
+<script setup>
+import ComponentStart from './components/ComponentStart.vue'
+
+
+</script>
+
+<style >
+html, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+.container {
+  max-width: 1100px;
+  box-sizing: content-box;
+  margin: 0 auto;
+  padding: 0 15px;
+  min-height: 100vh;
+}
+</style>
